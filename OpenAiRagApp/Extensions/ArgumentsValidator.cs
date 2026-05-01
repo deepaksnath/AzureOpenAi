aba94f989e1b6bfb386df3206e2cba481b298ee5
deepaksnath <deepak.s.nath@gmail.com>
2026-05-01 10:02:24 +0530

    Arguments validations added.

---
+++ OpenAiRagApp/Extensions/ArgumentsValidator.cs
@@ -0,0 +1,21 @@
+﻿using static OpenAiRagApp.Extensions.Constants;
+
+namespace OpenAiRagApp.Extensions
+{
+    static class ArgumentsValidator
+    {
+        public static bool ValidateArguments(string[] args, out string action, out bool isSeedingNeeded)
+        {
+            string firstArg = args?.Length > 0 ? args[0] : string.Empty;
+
+            if (Enum.TryParse(firstArg, true, out Mode mode))
+                action = mode.ToString();
+            else
+                action = Mode.INVALID.ToString();
+
+            isSeedingNeeded = args?.Length > 1 && args[1] == "1";
+
+            return action != Mode.INVALID.ToString();
+        }
+    }
+}
